3903c1d674a873a8008cb93266f70b587e57f6d4
Peter Rybar <pr.rybar@gmail.com>
2017-10-14 14:17:15 +0200

    server authbasic setup req.user

--- server/src/middleware/authbasic.ts
+++ server/src/middleware/authbasic.ts
@@ -1,27 +1,29 @@
 
 import * as log4js from "log4js";
-const log = log4js.getLogger("auth");
+const log = log4js.getLogger("authbasic");
 
 import * as basicAuth from "express-basic-auth";
 import * as tdb from "../db";
 import { User } from "../model/model";
 
-
-export const authBasic = basicAuth({
-    authorizeAsync: true,
-    authorizer: (username, password, cb) => {
-        tdb.get().collection("users").findOne({ login: username },
-            (err, user: User) => {
-                if (err) {
-                    log.error(err);
-                    cb(null, false);
-                } else {
-                    const auth = user && user.password === password;
-                    log.info("auth: ", username, password, user);
-                    cb(null, auth);
-                }
-            });
-    },
-    challenge: true,
-    realm: "Imb4T3st4pp"
-});
+export const authBasic = (req, res, next) => {
+    basicAuth({
+        authorizeAsync: true,
+        authorizer: (username, password, cb) => {
+            tdb.get().collection("users").findOne({ login: username },
+                (err, user: User) => {
+                    if (err) {
+                        log.error(err);
+                        cb(null, false);
+                    } else {
+                        const auth = user && user.password === password;
+                        log.info("auth: ", username, password, user);
+                        req.user = user;
+                        cb(null, auth);
+                    }
+                });
+        },
+        challenge: true,
+        realm: "Imb4T3st4pp"
+    })(req, res, next);
+};
